8d9430c559993f2960129bc7431489806d03d94b
RoBoT J Tech <RoBoTJTech@users.noreply.github.com>
2018-12-14 12:32:59 -0600

    Create html2php.php

---
+++ html2php.php
@@ -0,0 +1,12 @@
+<?php
+// Set base dir for website
+$baseDir='..';
+
+chdir($baseDir);
+$url = parse_url ($_SERVER ['REQUEST_URI']);
+$path=$url['path'];
+if (substr($path, -1)=='/')
+$path.="index.html";
+chdir('.'.dirname($path));
+$_SERVER['SCRIPT_NAME']=$path;
+include(basename($path));
